5151f8bf70ae8812c52afe02eab264866c97a096
ZixHD <havos25@gmail.com>
2025-06-11 23:40:31 +0200

    Added trashcan and toychest

--- window.gd
+++ window.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
-
+var can_add_text: bool = true;
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
+	Textbox.dialog_started.connect(_on_dialog_started)
+	Textbox.dialog_finished.connect(_on_dialog_finished)
+	
+func _on_dialog_started():
+	can_add_text = false
+	
+func _on_dialog_finished():
+	can_add_text = true
 	
 func _on_interact():
 	var text: String = DialogManager.get_message("window");
 	print(text)
-	Textbox.queue_text(text)
+	if(can_add_text):
+		Textbox.queue_text(text)
 	
